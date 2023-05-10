110bb92a6eced94b162b6fa40b9819472b87992b
Zdenko Hraste <zdenko.hraste@istea.com.ar>
2023-05-10 21:26:51 +0000

    cambio en el app.run

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def get_pokemon():
 
 port = int(os.environ.get('PORT', 5000)) 
 if __name__ == '__main__':
-	app.run(threaded=True, port=port, debug=True)
+	app.run(thre=True, port=port, debug=True)
